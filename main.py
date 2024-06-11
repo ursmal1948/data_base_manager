197ac42dd9cb8beda62657cb39681622a63c8586
ursmal1948 <ula.malin35@gmail.com>
2024-06-11 14:22:21 +0200

    main created

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import sqlite3
-from app.service import Offer
+from app.service import Offer, TravelAgenciesService
+from app.service import Continent
 
 
 def main() -> None:
     data_conn = Offer.connect_data('data/travel_agencies.txt')
-    print(data_conn)
+    # print(data_conn)
+    # print(TravelAgenciesService.get_last_travel_agency_id('datadata/travel_agencies.txt'))
+    # print(TravelAgenciesService.add_travel_agency('data/travel_agencies.txt', 'AOTHER NAMEE', 'DUBLIN'))
+    #     print(TravelAgenciesService.add_travel_agency('data/empty.txt', 'AOTHER NAMEE', 'DUBLIN'))
+    print(data_conn.get_trips_by_continent(Continent.EUROPE))
+    # print(data_conn.get_p(Continent.EUROPE))
 
 
 if __name__ == '__main__':
